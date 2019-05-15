f9617270c31f1b15dbb52b26977daec012063ea9
mhoeger <marie.hoeger@gmail.com>
2019-05-14 17:50:14 -0700

    actual working sample for express

--- express-example/app.ts
+++ express-example/app.ts
@@ -1,14 +1,15 @@
 import { FunctionApp } from "../future_node_modules/functions/azure-functions"
-const createHandler = require("azure-function-express").createHandler;
 const express = require("express");
 
 // Create express app as usual
 const app = express();
+
+app.get("/api/:foo", (req, res) => {
+    res.json({ foo: req.params.foo });
+});
+
 app.get("/api/:foo/:bar", (req, res) => {
-  res.json({
-    foo  : req.params.foo,
-    bar  : req.params.bar
-  });
+    res.json({ foo: req.params.foo, bar: req.params.bar });
 });
 
 export = new FunctionApp(app);
